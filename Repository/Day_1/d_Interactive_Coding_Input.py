5bb98b65c257d6929654010345891f9326c26a26
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-05-13 18:36:15 +0800

    Create d_Interactive_Coding_Input.py

---
+++ Repository/Day_1/d_Interactive_Coding_Input.py
@@ -0,0 +1,13 @@
+"""
+Write a program that prints the number of characters in a user's name. You might need to Google for a function that calculates the length of a string. 
+**Warning.** Your program should work for different inputs. e.g. any name that you input.
+
+Hint
+
+1. You can put functions inside other functions.
+2. Don't try to print anything other than the length.
+"""
+
+# Solution:
+
+print(len(input("Your Name: ")))
