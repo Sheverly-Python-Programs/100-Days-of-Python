c7027d6565ff6109a9c0123e815707e781e169bc
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-05-13 11:48:21 +0800

    Create b_string_manipulation.py

---
+++ Repository/Day_1/b_string_manipulation.py
@@ -0,0 +1,4 @@
+# String Manipulation and Code Intelligence - Sample of String Concatenation
+print("Hello " + "Anne") # -- Add a space after the first word.
+print("Hello" + " Anne") # -- Add a space before the second word.
+print("Hello" + " " + "Anne") # -- Add a space in between or what we call String Concatenation.
