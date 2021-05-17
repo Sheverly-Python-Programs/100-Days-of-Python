52643e1fbb424d02da3a879c6489f25fdc5a3f0c
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-05-17 19:29:53 +0800

    Create a_Conditional_Statement_Rollercoaster_app.py

---
+++ Repository/Day_3/a_Conditional_Statement_Rollercoaster_app.py
@@ -0,0 +1,6 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+if height >= 120:
+  print("Congratulations! You can ride the rollercoaster.")
+else:
+  print("Sorry, you are below the height requirement.\nPlease come back once you become taller.")
