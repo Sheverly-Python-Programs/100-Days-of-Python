3bfe1df7bff7a3afad52c2380d4818de3589b014
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-05-27 19:37:59 +0800

    Create a_For_Loops.py

---
+++ Repository/Day_5/a_For_Loops.py
@@ -0,0 +1,17 @@
+fruits = ["Apple", "Peach", "Pear"]
+for fruit in fruits:
+    print(fruit)
+    print(fruit + " pie")
+    
+'''
+Answer:
+Apple
+Apple pie
+Peach
+Peach pie
+Pear
+Pear pie
+'''
+
+####################################################################################################################################
+
