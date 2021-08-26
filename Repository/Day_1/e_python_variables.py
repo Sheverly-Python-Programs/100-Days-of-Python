223509129907aaaf01d8c0ed55b5adbc28ccba27
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-08-26 16:13:10 +0800

    Update e_python_variables.py

--- Repository/Day_1/e_python_variables.py
+++ Repository/Day_1/e_python_variables.py
@@ -5,3 +5,23 @@ This is the same as the previous len() function topic. But this time, variable/s
 name = input("What is your name? ")
 length = len(name) #this is to count the number of characters in "name" variable
 print(length) # this is to print the result/total of length
+
+# 🚨 Don't change the code below 👇
+a = input("a: ")
+b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+c = a
+a = b
+b = c
+
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+print("a: " + a)
+print("b: " + b)
+
+
