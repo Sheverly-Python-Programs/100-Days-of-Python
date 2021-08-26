d11c336864b7e164888e3de4961cb853ac78555b
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-08-26 15:37:28 +0800

    Update b_string_manipulation.py

--- Repository/Day_1/b_string_manipulation.py
+++ Repository/Day_1/b_string_manipulation.py
@@ -1,4 +1,12 @@
-# String Manipulation and Code Intelligence - Sample of String Concatenation
+# String Manipulation
+print("Hello World!\nHello World!\nHello World!")
+
+# Code Intelligence - Sample of String Concatenation
 print("Hello " + "Anne") # -- Add a space after the first word.
 print("Hello" + " Anne") # -- Add a space before the second word.
 print("Hello" + " " + "Anne") # -- Add a space in between or what we call String Concatenation.
+
+"""
+String concatenation is simply taking those separate strings of characters
+and merging them into one.
+"""
