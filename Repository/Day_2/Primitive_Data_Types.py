dd833a34df15fa4e9550192f23bef69a421619d9
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-08-27 05:24:50 +0800

    Create Primitive_Data_Types.py

---
+++ Repository/Day_2/Primitive_Data_Types.py
@@ -0,0 +1,13 @@
+#Data Types
+
+#Subscripting - it always starts with 0
+print("Sheverly"[0])
+#result 0 = S
+
+#Integer - Whole numbers
+print(123 + 345)
+# Large Numbers, instead of using commas, you may use an underscore.
+
+#Floating (Any number with Decimal Point)
+
+#Boolean (True and False) no quotation marks, nor anything.
