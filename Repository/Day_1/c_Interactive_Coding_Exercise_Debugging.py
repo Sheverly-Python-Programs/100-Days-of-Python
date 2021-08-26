cdb9909516c30e11f1bf7408993823baad839f06
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-08-26 15:41:08 +0800

    Update c_Interactive_Coding_Exercise_Debugging.py

--- Repository/Day_1/c_Interactive_Coding_Exercise_Debugging.py
+++ Repository/Day_1/c_Interactive_Coding_Exercise_Debugging.py
@@ -8,19 +8,8 @@ print(("New lines can be created with a backslash and n.")
 
 # Solution👇
 
+#Fix the code below 👇
 print("Day 1 - String Manipulation")
 print('String Concatenation is done with the "+" sign.')
 print('e.g. print("Hello " + "world")')
 print("New lines can be created with a backslash and n.")
-      
-# Test Your Code
-
-# Before checking the solution, try copy-pasting your code into this repl: 
-
-# [https://repl.it/@appbrewery/day-1-2-test-your-code](https://repl.it/@appbrewery/day-1-2-test-your-code)
-
-# This repl includes my testing code that will check if your code meets this assignment's objectives. 
-
-# Solution
-
-# [https://repl.it/@appbrewery/day-1-2-solution](https://repl.it/@appbrewery/day-1-2-solution)
