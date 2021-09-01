15ef55ab359bb42454b61a6272387cc5975e373f
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-09-02 01:54:16 +0800

    Update c_BMI_Calculator_Challenge.py

--- Repository/Day_2/c_BMI_Calculator_Challenge.py
+++ Repository/Day_2/c_BMI_Calculator_Challenge.py
@@ -55,3 +55,13 @@ bmi_as_int = int(bmi)
 print(bmi_as_int)
 
 ########################################################################################################################
+
+# 🚨 Don't change the code below 👇
+height = input("enter your height in m: ")
+weight = input("enter your weight in kg: ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+BMI = int(weight) / float(height) ** 2
+BMI_Whole_Number = int(BMI)
+print(BMI_Whole_Number)
