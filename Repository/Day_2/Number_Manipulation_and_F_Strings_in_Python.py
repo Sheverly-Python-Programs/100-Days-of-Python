fb86ef55403df799aa0d9be544f2e2c456681018
Sheverly Anne Aquino <54752276+DL06@users.noreply.github.com>
2021-09-02 15:23:06 +0800

    Create Number_Manipulation_and_F_Strings_in_Python.py

---
+++ Repository/Day_2/Number_Manipulation_and_F_Strings_in_Python.py
@@ -0,0 +1,18 @@
+print(round(8 / 3)) #The round function is used to round off numbers
+# The final answer is 3
+
+print(round(8 / 3, 2)) # The ", 2" means that you want to get the number of the decimal places
+# The final answer is 2.67
+
+print(8 // 3) # The "//" stands for floor division. It will automatically convert a floating point number into an integer.
+# The answer is 2
+
+################################################### Executed in Python GUI ###########################################################################################
+>>> score = 0
+>>> height = 1.8
+>>> isWinning = True
+>>> # This is f-string
+>>> print(f"Your score is {score}, your height is {height}, your status is {isWinning}!")
+Your score is 0, your height is 1.8, your status is True!
+#####################################################################################################################################################################
+
